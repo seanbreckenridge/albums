02a2d60a177cafa015570fc105a5bf211a7f9c23
Sean Breckenridge <seanbrecke@gmail.com>
2018-03-28 05:36:04 -0700

    formatting, comments

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
     flags = None
 
 # If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+# at ~/.credentials/sheets.googleapis.com-python-nextalbums.json
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Next Albums'
@@ -24,7 +24,8 @@ home_dir = os.path.expanduser('~')
 credential_dir = os.path.join(home_dir, '.credentials')
 if not os.path.exists(credential_dir):
     os.makedirs(credential_dir)
-credential_path = os.path.join(credential_dir,'sheets.googleapis.com-python-nextalbums.json')
+credential_path = os.path.join(credential_dir,
+                               'sheets.googleapis.com-python-nextalbums.json')
 store = Storage(credential_path)
 credentials = store.get()
 if not credentials or credentials.invalid:
@@ -32,7 +33,7 @@ if not credentials or credentials.invalid:
     flow.user_agent = APPLICATION_NAME
     if flags:
         credentials = tools.run_flow(flow, store, flags)
-    else: # Needed only for compatibility with Python 2.6
+    else:  # Needed only for compatibility with Python 2.6
         credentials = tools.run(flow, store)
     print('Storing credentials to ' + credential_path)
 else:
