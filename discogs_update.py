84ffa788d7745b3e1c89d84cad0f80bb3cd61472
Sean Breckenridge <seanbrecke@gmail.com>
2018-04-24 03:35:50 -0700

    refactored
    reordered try/catch blocks, use '|' instead of commas to prevent google sheets from treating 'USER_ENTERED' artist IDs as numbers, refactor code further into helper functions.

--- discogs_update.py
+++ discogs_update.py
@@ -14,11 +14,10 @@ import traceback
 from urllib.parse import urlparse
 from json import load
 from time import sleep
-from termcolor import colored
 
 spreadsheet_id = '12htSAMg67czl8cpkj1mX0TuAFvqL_PJLI4hv1arG5-M'
 d_Client = None
-update_threshold = 10  # ends the program and updates after these many updates
+update_threshold = 10   # ends the program and updates after these many updates
 update_count = 0
 
 
@@ -39,14 +38,14 @@ def init_discogs_client():
 def has_discogs_link(row):
     """Returns True/False based on whether the entry has a discogs link."""
     if len(row) >= 8:
-        return bool(row[7].strip())
+        return len(row[7].strip()) > 0
     return False
 
 
 def has_discogs_data(row):
     """Returns True if discogs data has been scraped for this entry before."""
     if has_discogs_link(row):
-        return bool(''.join(map(str, row[8:])).strip())
+        return bool("".join(map(str.strip, row[8:])))
     return False
 
 
@@ -61,6 +60,12 @@ def discogs_get(_type, id):
         return d_Client.master(id).main_release
     elif _type == "release":
         return d_Client.release(id)
+        sleep(2)
+        if rel.master is not None:
+            return rel.master.main_release
+        else:
+            print(f"Release {id} has no master release.")
+            return rel
     else:
         raise Excepion(f"Unknown discogs request type: {_type}")
 
@@ -72,18 +77,28 @@ def fix_discogs_link(link):
     if master_id:  # if we matched master id
         return "https://www.discogs.com/master/{}".format(master_id.groups()[0])
     else:  # if there is no master id
-        print(f"Warning: {link} is not a master link.")
         release_id = master_id = re.search("\/release\/(?:view\/)?(\d+)", urlparse_path)
         if release_id:
-            return "https://www.discogs.com/release/{}".format(release_id.groups()[0])
+            release_match = release_id.groups()[0]
+            print(f"Attempting to resolve release {release_match} to master.")
+            rel = d_Client.release(int(release_match))
+            sleep(2)
+            if rel.master is not None:
+                print(f"Resolved release {release_match} to {rel.master.id}.")
+                return "https://www.discogs.com/master/{}".format(int(rel.master.id))
+            else:
+                return "https://www.discogs.com/release/{}".format(release_match)
         else:
             raise Exception(f"Unknown discogs link: {link}. Exiting...")
 
 
-def fix_discogs_artist_name(name):
+def fix_discogs_artist_name(artists):
     """Fixes names if there are duplicates on Discogs.
     Discogs lists some artists with parens after their name to prevent duplicates."""
-    return re.sub(r"\(\d+\)$", "", name).strip()
+    artist_ids = [str(a.id) for a in artists]
+    artists_names = [str(a.name) for a in artists]
+    artist_fixed_names = [re.sub(r"\(\d+\)$", "", name).strip() for name in artists_names]
+    return ", ".join(artist_fixed_names), "|".join(artist_ids)
 
 
 def prompt_changes(old_row, new_row):
@@ -97,68 +112,82 @@ def prompt_changes(old_row, new_row):
                 and str(old_item).strip().lower() != str(new_item).strip().lower():
             changes.append(f"'{old_item}' → '{new_item}'")
     if changes:
-        print("\n".join([colored("CONFIRM CHANGES:", "red")] + changes), end="\n\n")
-        return strtobool(input("Continue with changes? "))
+        print("\n".join([colored("CONFIRM CHANGES:", "red")] + changes))
+        return strtobool(input("Confirm Changes? "))
     else:  # changes don't have to be confirmed, continue with changes
         return True
 
 
-def update_row_with_discogs_data(row):
+def update_row_with_discogs_data(row, max_length):
     """Gets values from discogs API and prompts the user to confirm changes."""
     global d_Client
     link = row[7]
     _type, id = urlparse(link).path.strip("/").split("/")
-    while len(row) <= 11:
-        row.append(None)
+    while len(row) < max_length:
+        row.append("")
     original_row = list(row)
     rel = discogs_get(_type, int(id))
     row[1] = rel.title  # Album Name
-    row[2] = fix_discogs_artist_name(rel.artists[0].name)  # Artist Name
+    row[2], row[8] = fix_discogs_artist_name(rel.artists)  # Artist Name
     row[3] = rel.year  # Year
     if row[3] == 0:  # discogs API returns 0 if year was unknown for master release
         print(f"Warning: Failed to get year for {id}: {row[1]}. Using old year ({original_row[3]}).")
         row[3] = original_row[3]
     row[6] = '=IMAGE("{}")'.format(rel.images[0]["uri"])  # Image
-    row[8] = ", ".join(sorted(set(rel.genres if rel.genres is not None else [])))  # Genres
-    row[9] = ", ".join(sorted(set(rel.styles if rel.styles is not None else [])))  # Style
+    row[9] = ", ".join(sorted(set(rel.genres if rel.genres is not None else [])))  # Genres
+    row[10] = ", ".join(sorted(set(rel.styles if rel.styles is not None else [])))  # Style
     artist_ids = set([person.id for person in rel.credits])
-    row[10] = ", ".join(map(str, artist_ids))  # Artist IDs
+    row[11] = "|".join(map(str, artist_ids))  # Credit Artist IDs
     if prompt_changes(original_row, row):
         return row
     else:
         return original_row
 
 
+def _fix_row(row, max_no_of_columns):
+    """Updates with Discogs data if necessary."""
+    if has_discogs_link(row):  # if this has a discogs link
+        row[7] = fix_discogs_link(row[7])  # fix link
+        if not has_discogs_data(row):  # if theres no data for this yet
+            row = update_row_with_discogs_data(row, max_no_of_columns)
+    return row
+
+
 def fix_rows(values):
-    """Error Handling; Calls 'update_row_with_discogs_data' for entries that need to be updated."""
+    """Error Handling, exits cleanly on exceptions."""
     global update_count
     header = values.pop(0)
+    max_no_of_columns = len(header)
+    all_links = set()
+
     for index, row in enumerate(values):
         row = list(map(str, row))
-        if update_count > update_threshold:  # if we've updated enough
+        if update_count > update_threshold:  # if we've updated enough entries
             print("Hit update threshold; updating spreadsheet.")
             break
-        if has_discogs_link(row):  # if this has a discogs link
-            row[7] = fix_discogs_link(row[7])
-            if not has_discogs_data(row):  # if this hasnt been updated with discogs data yet
-                try:
-                    row = update_row_with_discogs_data(row)  # update it
-                except discogs_client.exceptions.HTTPError as discogs_api_limit:  # wait on 429
-                    if '429' in str(discogs_client.exceptions.HTTPError):
-                        wait_time = 30
-                        print(f"[Discogs] API Limit Reached. Waiting {wait_time} seconds...")
-                        sleep(wait_time)
-                        try:
-                            row = update_row_with_discogs_data(row)
-                        except discogs_client.exceptions.HTTPError as second_error:
-                            print(second_error)
-                            break  # if an API error happened twice, leave the program
-                    else:
-                        raise(discogs_api_limit)  # Non rate-limit error
-                except Exception as e:
-                    traceback.print_exc()
-                    break
-        values[index] = row   # replace row back into values
+
+        try:
+            values[index] = _fix_row(row, max_no_of_columns)
+        except discogs_client.exceptions.HTTPError as discogs_api_limit:
+            if '429' in str(discogs_client.exceptions.HTTPError):
+                wait_time = 30
+                print(f"[Discogs] API Limit Reached. Waiting {wait_time} seconds...")
+                sleep(wait_time)
+            else:
+                traceback.print_exc() # non rate-limit related error
+                break
+        except:
+            traceback.print_exc() # other error
+            break
+
+        # check for duplicate links, i.e. duplicate entries
+        if has_discogs_link(row):
+            if row[7] not in all_links:
+                all_links.add(row[7])
+            else:  # exit if theres a duplicate discogs link (meaning duplicate entry)
+                print(f"Found duplicate of {row[7]}. Exiting...")
+                break
+
     values.insert(0, header)  # put header back
     return values
 
@@ -169,12 +198,13 @@ def get_values(credentials):
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
     service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
     # valueRendorOption = FORMULA is needed so we can keep images. Cells are blank or else.
-    result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range="A1:K", valueRenderOption="FORMULA").execute()
+    result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range="A1:L", valueRenderOption="FORMULA").execute()
     return result.get('values', [])
 
 
 def update_values(values, credentials):
     """Updates the values on the spreadsheet"""
+    # Uses batchUpdate instead of update since its difficult to format listened on from FORMULA valueRenderOption
     service = discovery.build('sheets', 'v4', credentials=credentials)
     no_of_rows = len(values)
     update_data = [
@@ -184,8 +214,8 @@ def update_values(values, credentials):
             'values': [vals[1:4] for vals in values]
         },
         {
-            # Album Artwork, Discogs Link, Genre, Style, Credits (ID)
-            'range': "Music!G1:K{}".format(no_of_rows),
+            # Album Artwork, Discogs Link, Artist ID(s), Genre, Style, Credits (ID)
+            'range': "Music!G1:L{}".format(no_of_rows),
             'values': [vals[6:] for vals in values]
         }
     ]
