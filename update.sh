9647f5b85aeb4a947a9cf2e99cc499287e035290
Sean Breckenridge <seanbrecke@gmail.com>
2019-10-23 13:06:30 -0700

    add new discogs link, some release -> master

--- update.sh
+++ update.sh
@@ -5,17 +5,17 @@
 # it may need to be called multiple times before its finished updating
 
 echo "Reminder: Run discogs_update.py and sort the spreadsheet online by Year, then Album Name"
-cd $(dirname "$0")
+cd "$(dirname "$0")" || exit
 echo "Generating csv..."
 python3 generate_csv.py
 echo "Creating SQL statements..."
-cd SQL
+cd SQL || exit
 python3 create_statements.py
 python3 create_statements.py --use-scores
 echo "updating albums..."
-cat statements.sql | sudo mysql
+sudo cat statements.sql | sudo mysql
 echo "updating scorealbums..."
-cat score_statements.sql | sudo mysql
-cd ../src
+sudo cat score_statements.sql | sudo mysql
+cd ../src || exit
 echo "Updating src csv files..."
 python3 _update_from_db.py
