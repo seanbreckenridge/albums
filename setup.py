2e47f6100dbe11fc68f6dc4a96828d947d11bbba
Sean Breckenridge <seanbrecke@gmail.com>
2021-04-05 17:08:16 -0700

    only run setup when running directly

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 requirements = Path("requirements.txt").read_text().strip().splitlines()
-
 pkg = "nextalbums"
-setup(
-    name=pkg,
-    version="0.1.0",
-    url="https://github.com/seanbreckenridge/albums",
-    author="Sean Breckenridge",
-    author_email="seanbrecke@gmail.com",
-    description=("my personal album system"),
-    license="MIT",
-    packages=find_packages(include=[pkg]),
-    entry_points={"console_scripts": ["nextalbums = nextalbums.__main__:main"]},
-    package_data={pkg: ["py.typed"]},
-    install_requires=requirements,
-)
+
+if __name__ == "__main__":
+    setup(
+        name=pkg,
+        url="https://github.com/seanbreckenridge/albums",
+        author="Sean Breckenridge",
+        author_email="seanbrecke@gmail.com",
+        description=("my personal album system"),
+        license="MIT",
+        packages=find_packages(include=[pkg]),
+        entry_points={"console_scripts": ["nextalbums = nextalbums.__main__:main"]},
+        package_data={pkg: ["py.typed"]},
+        install_requires=requirements,
+    )
